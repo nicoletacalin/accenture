862fe2ee6f18c0362b11a7f5b5003bdffecd2a8b
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-06-11 16:51:04 +0800

    Updates

---
+++ precache-manifest.314ed2783afb76cbdfd626b185dc8ce1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d0c8c3618c4becb9a61",
+    "url": "/nicoletacalin/accenture/static/css/main.40e2213b.chunk.css"
+  },
+  {
+    "revision": "7d0c8c3618c4becb9a61",
+    "url": "/nicoletacalin/accenture/static/js/main.7d0c8c36.chunk.js"
+  },
+  {
+    "revision": "5bcad4b3fae8e6759a22",
+    "url": "/nicoletacalin/accenture/static/css/1.f7736ec1.chunk.css"
+  },
+  {
+    "revision": "5bcad4b3fae8e6759a22",
+    "url": "/nicoletacalin/accenture/static/js/1.5bcad4b3.chunk.js"
+  },
+  {
+    "revision": "aec1c1046244e336708d",
+    "url": "/nicoletacalin/accenture/static/js/runtime~main.aec1c104.js"
+  },
+  {
+    "revision": "78995b80c5cd5a1b46086406d23f3dd3",
+    "url": "/nicoletacalin/accenture/static/media/AI.78995b80.jpeg"
+  },
+  {
+    "revision": "885494c4a238ea058a2b54d2659dd8c5",
+    "url": "/nicoletacalin/accenture/static/media/office.885494c4.jpeg"
+  },
+  {
+    "revision": "1e2e1dc71fd681682c072551d1723971",
+    "url": "/nicoletacalin/accenture/static/media/sweet.1e2e1dc7.jpeg"
+  },
+  {
+    "revision": "5e54eda5301d807a5b299f1f8970381e",
+    "url": "/nicoletacalin/accenture/static/media/accenture-logo.5e54eda5.png"
+  },
+  {
+    "revision": "ede1334484f87725b8b50bcc4f3a6718",
+    "url": "/nicoletacalin/accenture/static/media/aboutA.ede13344.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/nicoletacalin/accenture/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/nicoletacalin/accenture/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/nicoletacalin/accenture/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/nicoletacalin/accenture/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2cfcf00f4a8544a4c7b34f1ade88ffc0",
+    "url": "/nicoletacalin/accenture/static/media/schoolforjustice-hero.2cfcf00f.jpg"
+  },
+  {
+    "revision": "1c096329cfe94feadbd93a82f6c69f93",
+    "url": "/nicoletacalin/accenture/static/media/damtotdamloop-hero.1c096329.jpg"
+  },
+  {
+    "revision": "d9f60784300d361e7bba0affaee700a0",
+    "url": "/nicoletacalin/accenture/index.html"
+  }
+];
